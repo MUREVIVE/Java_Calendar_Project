3cdcd85be91d76ed5422f2edd8ca434a9dd3fab1
박영도 <@DESKTOP-O8LUM10>
2019-07-31 11:20:25 +0900

    정교한 달력 찍기

--- src/java_Calendar/Calendar.java
+++ src/java_Calendar/Calendar.java
@@ -1,14 +1,15 @@
 package java_Calendar;
 
-import java.util.Scanner;
 
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final String[] DAYS = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
 	
 	public boolean isLeapYear(int year) {
 		
+		// 윤년 주기
 		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
 		}
@@ -25,25 +26,54 @@ public class Calendar {
 	}
 
 	/*
-	 * - 월을 입력하면 해당월의 달력을 출력한다.
-	 * - 달력은 모양은 1단계에서 작성한 모양으로 만든다.
-	 * - 1일은 일요일로 정해도 무방하다.
-	 * - -1을 입력받기 전까지 반복 입력받는다.
-	 * - 프롬프트를 출력한다.
+	 	년도를 입력하세요.
+	 	YEAR > 2019
+	 	달을 입력하세요.
+	 	MONTH > 7
+	 	첫 번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)
+	 	WEEKDAY > WE
+	 	
+	 	<< 2019 7월 >>
+	 	SU MO TU WE TH FR SA
+	 	----------------------
+	 			 1  2  3  4
+	 	5  6  7  8  9  10  11
 	 */
 	
-	public void printCalendar(int year, int month) {
-		System.out.printf("<<%4d%3d>>\n", year, month);
-		System.out.println("  SU  MO  TU  WE  TH  FR  SA");
-		System.out.println("---------------------------");
+	
+	public void printCalendar(int year, int month, int weekday) {
+		System.out.printf("	   <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		
+		
+		
+		// print blank space
+		for(int i=0; i< weekday; i++) {
+			System.out.print("   ");
+		}
 		
 		int maxDay = getmaxDaysofMonth(year, month);
+		int count = 7-weekday;
+		int delim = (count < 7) ? count : 0;
 		
-		for(int i=1; i<= maxDay; i++) {
-			System.out.printf("%3d ", i);
-			if(i%7 == 0)
+		// print first line
+		for(int i=1; i<=count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+		
+		// print from second line to last
+		
+		count++;
+		for(int i=count; i<=maxDay; i++) {
+			System.out.printf("%3d", i);
+			if(i % 7 == delim)
 				System.out.println();
 		}
+		System.out.println();
+		System.out.println();
+		
 //		System.out.println("1 2 3 4 5 6 7");
 //		System.out.println("8 9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
@@ -51,8 +81,6 @@ public class Calendar {
 //		System.out.println("29 30 31");
 	}
 	public static void main(String[] args) {
-		
-		
 		
 		
 
